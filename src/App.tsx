150b467b6516e7ef63ee7abced4cf0d19f13d08f
unknown <vimalthanth70@gmail.com>
2024-09-13 09:39:55 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,88 @@
-import { useCallback, useEffect, useReducer, useRef, useState } from 'react'
-import './App.css'
-import CakeContainer from './components/CakeContainer'
-import { ErrorBoundary } from './components/ErrorBoundry'
-import MainCompo from './components/MainCompo'
-import store from './redux/store'
-import { Provider } from 'react-redux'
+import { useCallback, useEffect, useReducer, useRef, useState } from "react";
+import "./App.css";
+import CakeContainer from "./components/CakeContainer";
+import { ErrorBoundary } from "./components/ErrorBoundry";
+import MainCompo from "./components/MainCompo";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
-import MyComponent from './components/MyComponent'
-import useFecthData from './components/useFecthData'
+import MyComponent from "./components/MyComponent";
+import useFecthData from "./components/useFecthData";
 
-let timer:any;
+let timer: any;
 
-interface State{
-  count:number
+interface State {
+  count: number;
 }
 
-interface Action{
-  type:"Increment" | "decremnt"
+interface Action {
+  type: "Increment" | "decremnt";
 }
 
-function reducer(state:State,action:Action){
-  switch(action.type){
-      case "Increment":
-          return state.count++
-      case "decremnt":
-          return state.count--
-      default:
-          return state
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case "Increment":
+      return state.count++;
+    case "decremnt":
+      return state.count--;
+    default:
+      return state;
   }
 }
 
 function App() {
-  const [state,dispatch] = useReducer<any>(reducer,{count:0})
+  const [state, dispatch] = useReducer<any>(reducer, { count: 0 });
 
-  const [searchTxt,setSearchTxt] = useState("")
+  const [searchTxt, setSearchTxt] = useState("");
 
-  const {data,error,loading} = useFecthData("https://jsonplaceholder.typicode.com/todos","GET")
-  console.log(data,error,loading,'data19')
-  
-  async function fetchData(query:string){
-    try{
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/'+query)
-      console.log(response)
-    }catch(err){
+  const { data, error, loading } = useFecthData(
+    "https://jsonplaceholder.typicode.com/todos",
+    "GET"
+  );
+  console.log(data, error, loading, "data19");
 
-    }
+  console.log("hello there");
+
+  async function fetchData(query: string) {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/" + query
+      );
+      console.log(response);
+    } catch (err) {}
   }
 
-  const handleChange = (value:string)=>{
-    if(timer) clearTimeout(timer)
-      timer = setTimeout(()=>{
-        fetchData(value)
-    },1000)
-  }
+  const handleChange = (value: string) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fetchData(value);
+    }, 1000);
+  };
 
   return (
     <Provider store={store}>
       <CakeContainer />
       <ErrorBoundary>
-      <MainCompo />
-
+        <MainCompo />
       </ErrorBoundary>
-      <input onChange={(e)=>{
-        handleChange(e.target.value)
-      }} placeholder='search...' />
+      <input
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+        placeholder="search..."
+      />
       <MyComponent />
-      
-      <button onClick={()=>{
-        //@ts-ignore
-        dispatch({type:"Increment"})
-        }}>-</button>
 
-      
+      <button
+        onClick={() => {
+          //@ts-ignore
+          dispatch({ type: "Increment" });
+        }}
+      >
+        -
+      </button>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
